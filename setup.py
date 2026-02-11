69b3fd51a24baf15c0196b447ab1b8b87f467df7
Rishabh Mishra <mishrarishabh145@gmail.com>
2026-02-11 17:20:47 +0530

    Add initial Jupyter notebook, requirements, and setup for the project

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A Python package for a machine learning project',
+    author='Rishabh Mishra'
+)
